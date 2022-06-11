f17a4d0a58b9c54f15364e502e5226371e14cd28
Zak <rusty_clarinet@yahoo.com>
2022-06-10 17:30:13 -0700

    update level

---
+++ Assets/_Scripts/PosessionSystem/PosessionRaycast.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PosessionRaycast : MonoBehaviour
+{
+    
+
+    private void TryPossess()
+    {
+        //raycast to a target direction
+
+        //get the hit character info
+        
+        ///character struct consist of:
+        ///character's game object (GameObject)
+        ///character's properties
+        ///character's currentController (enum)
+        ///
+
+        if(true)
+        {
+            TryPossess();
+        }
+    }
+
+    private void Possess()
+    {
+        //character's currentController = this
+    }
+}
