833a28590602353f4089c817307cb4b14e5189d6
Varghese Bobus <varghesebobus@gmail.com>
2022-06-13 15:52:23 -0700

    Crosshair & Raycast selection

--- Assets/_Scripts/PosessionSystem/PosessionRaycast.cs
+++ Assets/_Scripts/PosessionSystem/PosessionRaycast.cs
@@ -4,8 +4,39 @@ using UnityEngine;
 
 public class PosessionRaycast : MonoBehaviour
 {
-    
+    [SerializeField] private Material highlightMaterial; 
+    [SerializeField] private string selectableTag = "Player";
+    [SerializeField] private Material defaultMaterial;
+    private Transform _selection;
 
+    private void Update()
+    {
+        if(_selection != null)
+        {
+            var selectionRenderer = _selection.GetComponent<Renderer>();
+            selectionRenderer.material = defaultMaterial;
+            _selection = null;
+        }
+
+        var ray = Camera.current.ScreenPointToRay(new Vector3(Screen.width/2f, Screen.height/2f, 0f));
+        RaycastHit hit;
+        if(Physics.Raycast(ray,out hit))
+        {
+            var selection = hit.transform;
+            if(selection.CompareTag(selectableTag))
+            {
+                 var selectionRenderer = selection.GetComponent<Renderer>();
+                if(selectionRenderer != null )
+                 {
+                    selectionRenderer.material = highlightMaterial;
+                 }
+
+                 _selection = selection;
+            }     
+
+        }
+    }
+    
     private void TryPossess()
     {
         //raycast to a target direction
