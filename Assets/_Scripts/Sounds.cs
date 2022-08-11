f951d1cb2a5a13fc07604ccda1df7fb9ffe748a6
Varghese Bobus <varghesebobus@gmail.com>
2022-08-11 11:27:11 -0700

    main bgm added

---
+++ Assets/_Scripts/Sounds.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sounds : MonoBehaviour
+{
+    private AudioSource PlayerAudio;
+    public AudioClip footstep;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerAudio = GetComponent<AudioSource>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
